df864f3f3ff764e7cf1eecd495914261abc87330
shane <sdrafahl@iastate.edu>
2017-03-11 22:23:58 -0600

    added command line arguments and readme

--- pi.c
+++ pi.c
@@ -5,15 +5,18 @@
 #include <unistd.h>
  typedef struct {
     /*Number of iterations that will process through infinite series*/
-     int iterations;
+     unsigned int iterations;
+     unsigned int precision;
      mpf_t added;
      mpf_t substracted;
 }DataShared;
 
-int DataSharedConst(DataShared* d){
-    d->iterations=20000000;
-    mpf_init2(d->added,1000);
-    mpf_init2(d->substracted,1000);
+int DataSharedConst(DataShared* d,unsigned itr,unsigned prec){
+    d->iterations=itr;
+    d->iterations=itr;
+    d->precision=prec;
+    mpf_init2(d->added,prec);
+    mpf_init2(d->substracted,prec);
     return 0;
 }
 
@@ -24,8 +27,8 @@ void *calc_add(void *arg){
     int x;
     mpf_t denom;
     mpf_t denomvalue;
-    mpf_init2(denomvalue,1000);
-    mpf_init2(denom,1000);
+    mpf_init2(denomvalue,data->precision);
+    mpf_init2(denom,data->precision);
     mpf_add_ui(denom,denom,1); 
     for(x=0;x<itr;x++){
         mpf_ui_div(denomvalue,1,denom);
@@ -43,8 +46,8 @@ void *calc_sub(void *arg){
     int x;
     mpf_t denom;
     mpf_t denom_val;
-    mpf_init2(denom_val,1000);
-    mpf_init2(denom,1000);
+    mpf_init2(denom_val,data->precision);
+    mpf_init2(denom,data->precision);
     mpf_add_ui(denom,denom,3); 
     for(x=0;x<itr;x++){
         mpf_ui_div(denom_val,1,denom);
@@ -54,7 +57,7 @@ void *calc_sub(void *arg){
     return NULL;
 }
   
-int main()
+int main(int argc, char *argv[])
 {
     /*Initial Setup*/
     
@@ -62,7 +65,9 @@ int main()
     pthread_t tid1;
     DataShared *cache;
     cache = malloc(sizeof(DataShared));
-    DataSharedConst(cache);
+    unsigned int itr = atoi(argv[1]);
+    unsigned int prec = atoi(argv[2]);
+    DataSharedConst(cache,itr,prec);
     /*Processing*/
     if(pthread_create(&tid, NULL, calc_add, cache)){
         return 1;
@@ -74,7 +79,7 @@ int main()
     pthread_join(tid1, NULL);
     /*Program Does Final Calculation*/
     mpf_t pi;
-    mpf_init2(pi,1000);
+    mpf_init2(pi,cache->precision);
     mpf_sub(pi,cache->added,cache->substracted);
     mpf_mul_ui(pi,pi,4);
     mp_exp_t exponent ;
